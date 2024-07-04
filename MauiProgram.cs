8286da45d2018ba536727cf60ef48846af364f56
Njay Garingarao <ninogaringarao13@gmail.com>
2024-07-04 23:55:54 +0800

    Chore (Designs and preset) : Design and preset related codes, files and nuget packages are installed and implemented from the original project

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace JRTV
 {
@@ -9,10 +10,12 @@ namespace JRTV
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkitMediaElement()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcon");
                 });
 
 #if DEBUG
